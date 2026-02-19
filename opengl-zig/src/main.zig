ddd066f34a579a87a122fb17b451a8c7b21ac439
BlueBlue21 <hanse3876@gmail.com>
2026-02-19 23:52:03 +0900

    Update main.zig

--- opengl-zig/src/main.zig
+++ opengl-zig/src/main.zig
@@ -6,10 +6,21 @@ const c = @cImport({
 });
 
 pub fn main() void {
-    _ = c.glfwInit();
+    if (c.glfwInit() == 0) {
+        std.debug.print("GLFW init failed.\n", .{});
+        return;
+    }
+    defer c.glfwTerminate();
+
     c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MAJOR, 3);
     c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 3);
     c.glfwWindowHint(c.GLFW_OPENGL_PROFILE, c.GLFW_OPENGL_CORE_PROFILE);
 
-    std.debug.print("Hello, GLFW!\n", .{});
+    const window = c.glfwCreateWindow(1600, 800, "Hello, OpenGL!", null, null);
+    if (window == null) {
+        std.debug.print("Window create failed.\n", .{});
+        return;
+    }
+
+    c.glfwMakeContextCurrent(window);
 }
