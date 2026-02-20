cf4082fbda3b512e7c3d2f6d8218ea21fced5908
BlueBlue21 <hanse3876@gmail.com>
2026-02-20 20:07:53 +0900

    🐈

--- README.txt
+++ README.txt
@@ -1,5 +1,7 @@
 https://zig.guide/language-basics/assignment/
+
 https://learnopenglkr.com/#hellowindow
+
 https://knarkzel.srht.site/os-in-zig/hello-world/
 https://wiki.osdev.org/Zig_Bare_Bones#build.zig
 
--- opengl-zig/src/main.zig
+++ opengl-zig/src/main.zig
@@ -5,8 +5,8 @@ const c = @cImport({
     @cInclude("GLFW/glfw3.h");
 });
 
-const WINDOW_WIDTH: i32 = 800;
-const WINDOW_HIGHT: i32 = 600;
+const window_width: i32 = 800;
+const window_height: i32 = 600;
 
 pub fn main() void {
     if (c.glfwInit() == 0) {
@@ -19,7 +19,7 @@ pub fn main() void {
     c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 3);
     c.glfwWindowHint(c.GLFW_OPENGL_PROFILE, c.GLFW_OPENGL_CORE_PROFILE);
 
-    const window = c.glfwCreateWindow(WINDOW_WIDTH, WINDOW_HIGHT, "Hello, OpenGL!", null, null);
+    const window = c.glfwCreateWindow(window_width, window_height, "Hello, OpenGL!", null, null);
     if (window == null) {
         std.debug.print("Window create failed.\n", .{});
         return;
@@ -33,15 +33,42 @@ pub fn main() void {
         return;
     }
 
-    c.glViewport(0, 0, WINDOW_WIDTH, WINDOW_HIGHT);
-    _ = c.glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    c.glViewport(0, 0, window_width, window_height);
+    _ = c.glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
     while (c.glfwWindowShouldClose(window) == 0) {
+        processInput(window);
+
+        c.glClearColor(0.2, 0.3, 0.3, 1.0);
+        c.glClear(c.GL_COLOR_BUFFER_BIT);
+
+        const vertices = [_]f32{
+            -0.5, -0.5, 0.0, // left
+            0.5, -0.5, 0.0, // right
+            0.0, 0.5, 0.0, //top
+        };
+
+        var vbo: u32 = undefined;
+        c.glGenBuffers(1, &vbo);
+        c.glBindBuffer(c.GL_ARRAY_BUFFER, vbo);
+
+        c.glBufferData(
+            c.GL_ARRAY_BUFFER,
+            @sizeOf(@TypeOf(vertices)),
+            &vertices,
+            c.GL_STATIC_DRAW,
+        );
+
         c.glfwSwapBuffers(window);
         c.glfwPollEvents();
     }
 }
 
-fn framebuffer_size_callback(_: ?*c.GLFWwindow, width: i32, height: i32) callconv(.c) void {
+fn framebufferSizeCallback(_: ?*c.GLFWwindow, width: i32, height: i32) callconv(.c) void {
     c.glViewport(0, 0, width, height);
+}
+
+fn processInput(window: ?*c.GLFWwindow) callconv(.c) void {
+    if (c.glfwGetKey(window, c.GLFW_KEY_ESCAPE) == c.GLFW_PRESS)
+        c.glfwSetWindowShouldClose(window, 1);
 }
